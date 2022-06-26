6198e4018edf06bcc2a9225f0812814ed5064080
Minh Nhat Doan <doannhatminh253@gmail.com>
2022-06-26 20:00:39 +0700

    mutation genetic algorithm

--- car.js
+++ car.js
@@ -13,10 +13,15 @@ class Car
         this.angle = 0;
         this.damage=false;
         this.polygon = this.#create_polygon();
+
+        this.useBrain = control_type=='AI';
         
         if(control_type != 'DUMMY')
         {
             this.sensor = new Sensor(this);
+            this.brain = new NeuralNetwork(
+                [this.sensor.ray_count, 6, 4]
+            );
         }
         this.controls = new Controls(control_type);
     }
@@ -31,7 +36,19 @@ class Car
         }
         if(this.sensor)
         {
-            this.sensor.update(road_borders, traffic);    
+            this.sensor.update(road_borders, traffic);   
+            const offsets = this.sensor.readings.map(
+                s=>s==null?0:1-s.offset
+            );
+            const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+
+            if(this.useBrain)
+            {
+                this.controls.forward=outputs[0];
+                this.controls.left=outputs[1];
+                this.controls.right=outputs[2];
+                this.controls.reverse=outputs[3];
+            }
         }
     }
 
@@ -133,7 +150,7 @@ class Car
         this.y -= Math.cos(this.angle) * this.speed;
     }
 
-    draw(ctx, color)
+    draw(ctx, color, drawSensor=false)
     {
         if(this.damage)
         {
@@ -150,7 +167,7 @@ class Car
             ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
         }
         ctx.fill();
-        if(this.sensor)
+        if(this.sensor && drawSensor)
         {
             this.sensor.draw(ctx);
         }
